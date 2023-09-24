3939fb2ed97fb819f2f0099ae4d787731244ef50
DHEEPA K <69415255+dheepa29@users.noreply.github.com>
2023-09-24 23:32:30 +0530

    Update InvoiceRepository.java

--- BBC/Backend/demo (1)/demo/src/main/java/com/BBC/service/InvoiceRepository.java
+++ BBC/Backend/demo (1)/demo/src/main/java/com/BBC/service/InvoiceRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.Query;
 import com.BBC.model.Invoice;
 
 public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
-	// Define the custom native query
+
     @Query(value = "SELECT * FROM invoice i WHERE (:customerId IS NULL OR i.customer_id = :customerId) AND (:status IS NULL OR i.status = :status)", nativeQuery = true)
 	List<Invoice> findByCustomerIdAndStatus(String customerId, String status);
     
